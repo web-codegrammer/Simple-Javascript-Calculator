97eb2906c5854d89017cc8bf255040bf1cdefc5c
Jatin Madaan <sjmadaan143@iiitsonepat.ac.in>
2024-01-11 22:52:38 +0530

    Upgraded functionality a to support more arithmetic operations including float and arithmetic expressions

--- script.js
+++ script.js
@@ -1,78 +1,82 @@
-function getHistory(){
-	return document.getElementById("history-value").innerText.replace("÷","/").replace("×","*");
+function getHistory() {
+  return document.getElementById("history-value");
 }
-function printHistory(num){
-	document.getElementById("history-value").innerText = num;
+function printHistory(num) {
+  document.getElementById("history-value").innerText = num;
 }
-function getOutput(){
-	return document.getElementById("output-value").innerText;
+function getOutput() {
+  return document.getElementById("output-value").innerText;
 }
-function printOutput(num){
-	if(num==""){
-		document.getElementById("output-value").innerText=num;
-	}
-	else{
-		document.getElementById("output-value").innerText=getFormattedNumber(num);
-	}	
+function printOutput(num) {
+  document.getElementById("output-value").innerText = num;
 }
-function getFormattedNumber(num){
-	if(num=="-"){
-		return "";
-	}
-	var n = Number(num);
-	var value = n.toLocaleString("en");
-	return value;
-}
-function reverseNumberFormat(num){
-	return Number(num.replace(/,/g,''));
-}
-var operator = document.getElementsByClassName("operator");
-for(var i =0;i<operator.length;i++){
-	operator[i].addEventListener('click',function(){
-		if(this.id=="clear"){
-			printHistory("");
-			printOutput("");
-		}
-		else if(this.id=="backspace"){
-			var output=reverseNumberFormat(getOutput()).toString();
-			if(output){//if output has a value
-				output= output.substr(0,output.length-1);
-				printOutput(output);
-			}
-		}
-		else{
-			var output=getOutput();
-			var history=getHistory();
-			if(output==""&&history!=""){
-				if(isNaN(history[history.length-1])){
-					history= history.substr(0,history.length-1);
-				}
-			}
-			if(output!="" || history!=""){
-				output= output==""?output:reverseNumberFormat(output);
-				history=history+output;
-				if(this.id=="="){
-					var result= eval(history);
-					printOutput(result);
-					printHistory("");
-				}
-				else{
-					history=history+this.id;
-					printHistory(history);
-					printOutput("");
-				}
-			}
-		}
-		
-	});
-}
-var number = document.getElementsByClassName("number");
-for(var i =0;i<number.length;i++){
-	number[i].addEventListener('click',function(){
-		var output=reverseNumberFormat(getOutput());
-		if(output!=NaN){ //if output is a number
-			output=output+this.id;
-			printOutput(output);
-		}
-	});
+var lst = ["%", "/", "+", "-", "*"];
+var f = false;
+var operator = document.getElementsByClassName("number");
+
+for (var i = 0; i < operator.length; i++) {
+  operator[i].addEventListener("click", function () {
+    if (this.id == "clear") {
+      printHistory("");
+      printOutput("");
+    } else if (this.id == "backspace") {
+      var output = getOutput();
+      if (output) {
+        //if output has a value
+        output = output.substring(0, output.length - 1);
+        //output = output;
+        printOutput(output);
+      }
+    } else if (this.id == "=") {
+      try {
+        var output = getOutput();
+
+        if (output == "NaN" || output == "" || f) {
+          f = false;
+          printOutput("");
+          printHistory("");
+        } else {
+          var outpu = eval(output);
+          // print(outpu);
+          if (outpu == undefined) {
+            outpu = "NaN";
+            output = "";
+          }
+          f = true;
+          printOutput(outpu);
+          printHistory(output);
+        }
+      } catch (error) {
+        outpu = "NaN";
+        output = "";
+        f = true;
+        printOutput(outpu);
+        printHistory(output);
+      }
+    } else {
+      var output = getOutput().toString();
+      //console.log(lst[1] == this.id);
+      if (output.length == 0 && lst.includes(this.id)) {
+        printOutput("");
+        printHistory("");
+        f = false;
+      } else {
+        if (f || output == "NaN") {
+          if (!lst.includes(this.id)) {
+            output = this.id;
+            printHistory("");
+          } else {
+            output = output + this.id.toString();
+          }
+          f = false;
+        } else {
+          if (output.length > 0) output = output + this.id.toString();
+          else output = this.id;
+        }
+        console.log(output);
+
+        printOutput(output);
+      }
+    }
+  });
 }
